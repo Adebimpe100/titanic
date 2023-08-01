b4c45fa2776324bb546356d3f8d71c2890d6f750
Adebimpe100 <137870935+Adebimpe100@users.noreply.github.com>
2023-08-01 12:58:07 +0100

    Update titanic_pred.py

--- titanic_pred.py
+++ titanic_pred.py
@@ -1,22 +1,22 @@
 # -->Importing libraries<--
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly as px
+#import matplotlib.pyplot as plt
+#import plotly as px
 import seaborn as sns; sns.set()
 from st_on_hover_tabs import on_hover_tabs
 
 
 #Imported models
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import confusion_matrix
+#from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.metrics import accuracy_score
+#from sklearn.tree import DecisionTreeClassifier
+#from sklearn import tree
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.model_selection import cross_val_score
 import base64
 import streamlit as st 
 import streamlit_extras as ste
