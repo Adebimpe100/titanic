4161b5260e42ab99d61c2020079ffb80a23967cf
Adebimpe100 <137870935+Adebimpe100@users.noreply.github.com>
2023-08-01 12:58:44 +0100

    Update titanic_pred.py

--- titanic_pred.py
+++ titanic_pred.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
 #import plotly as px
-import seaborn as sns; sns.set()
+#import seaborn as sns; sns.set()
 from st_on_hover_tabs import on_hover_tabs
 
 
